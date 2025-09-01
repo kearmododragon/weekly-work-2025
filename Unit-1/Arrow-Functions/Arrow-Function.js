cad0d337e50fa9aa679e15f723b73aebb9479d22
Kearney <kearnec@emea.corpdir.net>
2025-09-01 10:20:32 +0200

    finished AF

---
+++ Unit-1/Arrow-Functions/Arrow-Function.js
@@ -0,0 +1,23 @@
+/* function hellowWorld(str) {
+    return `Hellow ${str}`
+}
+
+console.log (hellowWorld("World")) */
+/* x => { ... }  // one parameter
+() => { ... }  // no parameters
+(x, y) => { ... }  // two or more parameters */
+
+const hellowWorld = (str) => {
+    return `Hello ${str}`
+}
+console.log(hellowWorld("howdy"))
+
+const imp = () => ({animal: 'dolphin', swims: true})
+console.log(imp())
+
+function checkArgs() { console.log(arguments); }
+/* checkArgs(1, 'abc') // outputs [1, "abc"] */
+
+/* const checkArgs = () => console.log(arguments);
+checkArgs(1, 'abc') // outputs Uncaught ReferenceError: arguments is not defined */
+
