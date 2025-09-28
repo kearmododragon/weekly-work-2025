f73b55899980771b89006923314fb8d5528b7ba4
kearmododragon <ckearney1992@gmail.com>
2025-09-28 20:32:08 +0200

    ex 2 done

--- Unit-1/Callback_Lab/Excercise2.js
+++ Unit-1/Callback_Lab/Excercise2.js
@@ -1,10 +1,22 @@
 // Exercise 2
-//
-// Filter the words array from above to create a new array
-// named longWords that includes only the words with 7 or more
-// characters
 
-const longWords = words.filter(/* write an anonymous inline function here */);
+// Javascript arrays have a built-in sort method that can take
+// a callback to tell it how to compare the things you want to sort.
 
-// Check that logging longWords outputs
-// ["lengthy", "delicious"]
+// Research the array sort method.
+
+// Write the sorting callback as a named function declaration
+
+// Write the callback function to provide to the sort method so that
+// the following code sorts the words from shortest to longest.
+
+const words = ['short', 'medium', 'delicious', 'nice', 'lengthy'];
+
+// The sort method sorts "in place", that is, it modifies the array
+words.sort(sortbylength);
+function sortbylength(a, b) {
+  return a.length - b.length
+}
+console.log(words)
+// Check that logging words now outputs
+// ["nice", "short", "medium", "lengthy", "delicious"]
--- Unit-1/Callback_Lab/Excercise3.js
+++ Unit-1/Callback_Lab/Excercise3.js
@@ -1,19 +1,10 @@
 // Exercise 3
-
-// Javascript arrays have a built-in sort method that can take
-// a callback to tell it how to compare the things you want to sort.
-
-// Research the array sort method.
-
-// Write the sorting callback as a named function declaration
-
-// Write the callback function to provide to the sort method so that
-// the following code sorts the words from shortest to longest.
-
+//
+// Filter the words array from above to create a new array
+// named longWords that includes only the words with 7 or more
+// characters
 const words = ['short', 'medium', 'delicious', 'nice', 'lengthy'];
+const longWords = words.filter(/* write an anonymous inline function here */);
 
-// The sort method sorts "in place", that is, it modifies the array
-words.sort(/* pass in a named callback here */);
-
-// Check that logging words now outputs
-// ["nice", "short", "medium", "lengthy", "delicious"]
+// Check that logging longWords outputs
+// ["lengthy", "delicious"]
